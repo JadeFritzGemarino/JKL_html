a5e72f8702ae068b99151b793a2ef4e1759f2662
Jade Fritz Gemarino <jadefritzgemarino@gmail.com>
2026-05-07 05:50:27 +0800

    Add bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,14 @@ module.exports = {
   content: ["./*.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {
+      backgroundImage: {
+        'jkl-hero': "url('/jkl_assets/jkl_bg.png')",
+      },
       colors: {
-        'jkl-dark': '#111111',  
-        'jkl-green': '#04AA6D', 
-        'jkl-gray': '#818181',   
-        'jkl-bg': '#f4f7f6',   
+        'jkl-dark': '#111111',
+        'jkl-green': '#04AA6D',
+        'jkl-gray': '#818181',
+        'jkl-bg': '#f4f7f6',
       },
     },
   },
